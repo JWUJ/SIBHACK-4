2f7c40ed850b226933d13a52e2d70e9c7dcf5c0a
Алексей <Stovovoy@ya.ru>
2023-11-12 02:46:29 +1000

    Добавил 2 тестовых страницы
    Одна получает excel и сохраняет в json
    Другая страница выводит данные по отчёту

--- pages/test.php
+++ pages/test.php
@@ -11,7 +11,68 @@ $reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
 //    $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
 /** Load $inputFileName to a Spreadsheet Object  **/
 $spreadsheet = $reader->load($inputFileName);
-foreach($spreadsheet->getActiveSheet()->toArray() as $value){
-    $str = implode('; ', $value);
-    echo "$str <br>";
+
+$worksheet = $spreadsheet->getActiveSheet();
+
+$data = [];
+$currentRow = null;
+
+foreach ($worksheet->getRowIterator() as $row) {
+    $cellIterator = $row->getCellIterator();
+    $rowIndex = $row->getRowIndex();
+    
+    $rowData = [];
+
+    foreach ($cellIterator as $cell) {
+        $cellValue = $cell->getValue();
+        $cellColor = $cell->getStyle()->getFill()->getStartColor()->getRGB();
+
+        if ($rowIndex >= 17 && $cellColor == 'B0C4DE') {
+            // Если цвет ячейки - красный и строка больше 17, начинаем новый подмассив
+            if ($currentRow !== null) {
+                $data[] = $currentRow;
+            }
+            $currentRow = [];
+        }
+
+        $rowData[] = $cellValue;
+    }
+
+    if ($currentRow !== null) {
+        $currentRow[] = $rowData;
+    }
 }
+
+if ($currentRow !== null) {
+    $data[] = $currentRow;
+}
+
+function array_filter_recursive($input) 
+{ 
+  foreach ($input as &$value) 
+  { 
+    if (is_array($value)) 
+    { 
+      $value = array_filter_recursive($value); 
+    } 
+  } 
+
+  return array_filter($input); 
+} 
+
+$data = array_filter_recursive($data);
+
+$data = array_values(array_filter($data, function($subarray) {
+    return !empty($subarray);
+}));
+
+
+//$data = array_filter_recursive($data);
+$jsonData = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+file_put_contents('output.json', $jsonData);
+
+echo 'Данные успешно преобразованы в JSON и сохранены в output.json.';
+
+echo '<pre>', print_r($data, true), '</pre>';
+?>
